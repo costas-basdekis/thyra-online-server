b531b2247360df76c090396e218f5c14bc49bae1
Costas Basdekis <costas.basdekis@gmail.com>
2019-08-07 10:30:00 +0100

    Allow connection from anywhere

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const app = require('express')();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+  //origins: '*:*',
+  handlePreflightRequest: (req, res) => {
+    const headers = {
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+        "Access-Control-Allow-Credentials": true
+    };
+    res.writeHead(200, headers);
+    res.end();
+  },
+});
 const uuid4 = require('uuid4');
 const {Game} = require('./game/game');
 const fs = require('fs');
