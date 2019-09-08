6a94546172e45f4248b01a924d3b81b21a9927a8
Costas Basdekis <costas.basdekis@gmail.com>
2019-09-08 21:04:47 +0100

    Make `listen` into a class

--- src/websocket/listen.js
+++ src/websocket/listen.js
@@ -1,65 +1,100 @@
 const emit = require('./emit');
 const {io} = require('./io');
 
+let {model, globalData} = {};
+
 const minAppVersion = 2;
 
-io.on('connection', function(socket){
-  const {model, persistence: {globalData}} = require('../data');
-  console.log('a user connected');
-  let user = null;
-  socket.on('create-user', ({appVersion, id, token} = {}) => {
+class Connection {
+  static connect = socket => {
+    if (!model) {
+      ({model, persistence: {globalData}} = require('../data'));
+    }
+    return new this(socket);
+  };
+
+  constructor(socket) {
+    console.log('a user connected');
+    this.socket = socket;
+    this.user = null;
+    this.setupCommands();
+  }
+
+  setupCommands() {
+    for (const [command, callback] of Object.entries(this.socketMap)) {
+      this.socket.on(command, callback);
+    }
+    this.socketMap = {};
+  }
+
+  socketMap = {};
+
+  on = (command, callback) => {
+    this.socketMap[command] = callback;
+    if (this.socket) {
+      this.setupCommands();
+    }
+    return callback;
+  };
+
+  createUser = this.on('create-user', ({appVersion, id, token} = {}) => {
     if (!appVersion) {
       console.log('user has app with no version');
       // Try anyway to make it reload
-      emit.askUserToReload(socket);
-      user = null;
+      emit.askUserToReload(this.socket);
+      this.user = null;
       return;
     }
     if (appVersion < minAppVersion) {
       console.log('user has old app version', {appVersion, minAppVersion});
-      emit.askUserToReload(socket);
-      user = null;
+      emit.askUserToReload(this.socket);
+      this.user = null;
       return;
     }
-    if (user) {
-      emit.emitUser(user);
-      emit.emitUsers(socket);
-      emit.emitGames(socket);
-      return;
-    }
-
-    [user] = model.loadOrCreateUser(id, token, socket);
-  });
-  socket.on('change-username', username => {
-    if (!user) {
+    if (this.user) {
+      emit.emitUser(this.user);
+      emit.emitUsers(this.socket);
+      emit.emitGames(this.socket);
       return;
     }
 
-    model.renameUser(user, username);
+    [this.user] = model.loadOrCreateUser(id, token, this.socket);
   });
-  socket.on('change-password', async password => {
-    if (!user) {
+
+  changeUsername = this.on('change-username', username => {
+    if (!this.user) {
       return;
     }
 
-    await model.changePassword(user, password);
+    model.renameUser(this.user, username);
   });
-  socket.on('update-settings', settings => {
-    if (!user) {
+
+  changePassword = this.on('change-password', async password => {
+    if (!this.user) {
       return;
     }
 
-    model.updateUserSettings(user, settings);
+    await model.changePassword(this.user, password);
   });
-  socket.on('change-ready-to-play', readyToPlay => {
-    if (!user) {
+
+  updateSettings = this.on('update-settings', settings => {
+    if (!this.user) {
       return;
     }
 
-    model.changeUserReadyToPlay(user, readyToPlay);
+    model.updateUserSettings(this.user, settings);
   });
-  socket.on('submit-game-moves', ({id, moves}) => {
-    if (!user) {
+
+  changeReadyToPlay = this.on('change-ready-to-play', readyToPlay => {
+    if (!this.user) {
+      return;
+    }
+
+    model.changeUserReadyToPlay(this.user, readyToPlay);
+  });
+
+  submitGameMoves = this.on('submit-game-moves', ({id, moves}) => {
+    if (!this.user) {
       return;
     }
     const game = globalData.games[id];
@@ -68,12 +103,15 @@ io.on('connection', function(socket){
       return;
     }
 
-    model.submitGameMoves(user, game, moves);
+    model.submitGameMoves(this.user, game, moves);
   });
-  socket.on('disconnect', () => {
-    if (user) {
-      model.disconnectUser(user, socket);
-      user = null;
+
+  disconnect = this.on('disconnect', () => {
+    if (this.user) {
+      model.disconnectUser(this.user, this.socket);
+      this.user = null;
     }
   });
-});
+}
+
+io.on('connection', Connection.connect);
