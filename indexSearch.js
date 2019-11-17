627d5d0bba084b0bbe3f2be8fa7bcd3377d91860
Costas Basdekis <costas.basdekis@gmail.com>
2019-11-17 17:51:07 +0000

    Fix `indexSearch.js`

--- indexSearch.js
+++ indexSearch.js
@@ -1,4 +1,4 @@
 const {MinimumGame} = require('./src/game/MinimumGame');
 
 ss1=MinimumGame.solvePosition({position: 'DAGMGAICBEAGAAAAAAAAAAAAD', maxDepth: 5, usePool: true});
-console.log(JSON.stringify(ss1.historyFullMoves));
+console.log(JSON.stringify(ss1.root.leaves.map(game => game.historyFullMoves)));
