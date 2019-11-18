5fe7ea045a60fc607810f862b8c2b6419f1e0841
Costas Basdekis <costas.basdekis@gmail.com>
2019-11-18 15:41:47 +0000

    Add more puzzles to search

--- indexSearch.js
+++ indexSearch.js
@@ -1,9 +1,25 @@
 const _ = require('lodash');
 const {MinimumGame} = require('./src/game/MinimumGame');
 
-ss1=MinimumGame.solvePosition({position: 'DAGMGAICBEAGAAAAAAAAAAAAD', maxDepth: 5, usePool: true});
+const puzzles = [
+  {position: 'DDAMGGMCCHAAGHAAAAADAAAAD', maxDepth: 1},
+  {position: 'MIMMAMHGMGMGGHGDMMIGAADAA', maxDepth: 3},
+  {position: 'DAGMGAICBEAGAAAAAAAAAAAAD', maxDepth: 5},
+  {position: 'JEJJGJACAEAAAFDGADAAAGAAA', maxDepth: 7},
+  {position: 'AGADAIACAADBAAAAADAAAAAED', maxDepth: 9},
+];
+const puzzle = puzzles[2];
+console.log(`Solving ${puzzle.position} with depth ${puzzle.maxDepth}`);
+ss1=MinimumGame.solvePosition({...puzzle, usePool: true});
 const allHistoryMoves = (ss1.root.leaves || []).map(game => game.historyFullMoves);
-const expectedHistoryMoves = [
+const expectedHistoryMoves = {
+  'DDAMGGMCCHAAGHAAAAADAAAAD': [
+    [[{"x":4,"y":1},{"x":4,"y":2},{"x":4,"y":1}]],[[{"x":3,"y":2},{"x":4,"y":2},{"x":3,"y":2}]],
+  ],
+  'MIMMAMHGMGMGGHGDMMIGAADAA': [
+    [[{"x":3,"y":2},{"x":2,"y":1},{"x":1,"y":2}],[{"x":3,"y":3},{"x":2,"y":2},{"x":1,"y":2}],[{"x":2,"y":1},{"x":3,"y":2},{"x":4,"y":1}]],
+  ],
+  'DAGMGAICBEAGAAAAAAAAAAAAD': [
   [
     [
       {
@@ -3388,10 +3404,17 @@ const expectedHistoryMoves = [
       }
     ]
   ]
-];
-console.log(JSON.stringify(allHistoryMoves));
-const missingMoves = _.differenceBy(expectedHistoryMoves, allHistoryMoves, historyMoves => JSON.stringify(historyMoves));
-const extraMoves = _.differenceBy(allHistoryMoves, expectedHistoryMoves, historyMoves => JSON.stringify(historyMoves));
-if (missingMoves.length || extraMoves.length) {
-  throw new Error(`Wrong set of moves returned: ${missingMoves.length} are missing, and ${extraMoves.length} are added`);
+],
+}[puzzle.position];
+if (expectedHistoryMoves) {
+  const missingMoves = _.differenceBy(expectedHistoryMoves, allHistoryMoves, historyMoves => JSON.stringify(historyMoves));
+  const extraMoves = _.differenceBy(allHistoryMoves, expectedHistoryMoves, historyMoves => JSON.stringify(historyMoves));
+  if (missingMoves.length || extraMoves.length) {
+    throw new Error(`Wrong set of moves returned: ${missingMoves.length} are missing, and ${extraMoves.length} are added`);
+  } else {
+    console.log(`Found ${allHistoryMoves.length} solutions, as expected`);
+  }
+} else {
+  console.log(`Found ${allHistoryMoves.length} solutions`);
+  console.log(JSON.stringify(allHistoryMoves));
 }
