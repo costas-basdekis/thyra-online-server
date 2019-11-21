4ebc2ca69af5e9f02e7b861f9516c24de68bff88
Costas Basdekis <costas.basdekis@gmail.com>
2019-11-21 17:18:37 +0000

    Add solution for puzzle #4

--- indexSearch.js
+++ indexSearch.js
@@ -3405,6 +3405,10 @@ const expectedHistoryMoves = {
     ]
   ]
 ],
+  'JEJJGJACAEAAAFDGADAAAGAAA': [
+    [[{"x":2,"y":1},{"x":1,"y":2},{"x":2,"y":3}],[{"x":4,"y":1},{"x":4,"y":0},{"x":4,"y":1}],[{"x":1,"y":2},{"x":2,"y":1},{"x":3,"y":0}],[{"x":4,"y":0},{"x":4,"y":1},{"x":4,"y":0}],[{"x":2,"y":1},{"x":3,"y":1},{"x":4,"y":0}],[{"x":1,"y":0},{"x":1,"y":1},{"x":0,"y":0}],[{"x":3,"y":2},{"x":2,"y":3},{"x":1,"y":4}]],
+    [[{"x":2,"y":1},{"x":2,"y":2},{"x":2,"y":3}],[{"x":4,"y":1},{"x":4,"y":0},{"x":4,"y":1}],[{"x":2,"y":2},{"x":2,"y":1},{"x":3,"y":0}],[{"x":4,"y":0},{"x":4,"y":1},{"x":4,"y":0}],[{"x":2,"y":1},{"x":3,"y":1},{"x":4,"y":0}],[{"x":1,"y":0},{"x":1,"y":1},{"x":0,"y":0}],[{"x":3,"y":2},{"x":2,"y":3},{"x":1,"y":4}]],
+  ],
 }[puzzle.position];
 if (expectedHistoryMoves) {
   const missingMoves = _.differenceBy(new Set(expectedHistoryMoves), new Set(allHistoryMoves), historyMoves => JSON.stringify(historyMoves));
