615110f735ceacb3b0784fc574308e8806e56a3f
Costas Basdekis <costas.basdekis@gmail.com>
2019-11-24 14:43:24 +0000

    Fix search checking code

--- indexSearch.js
+++ indexSearch.js
@@ -9,9 +9,6 @@ const puzzles = [
   {position: 'AGADAIACAADBAAAAADAAAAAED', maxDepth: 9},
 ];
 const puzzle = puzzles[4];
-console.log(`Solving ${puzzle.position} (#${MinimumGame.fromPosition(puzzle.position).hash}) with depth ${puzzle.maxDepth}`);
-ss1=MinimumGame.solvePosition({...puzzle, usePool: true});
-const allHistoryMoves = (ss1.root.leaves || []).map(game => game.historyFullMoves);
 const expectedHistoryMoves = {
   'DDAMGGMCCHAAGHAAAAADAAAAD': [
     [[{"x":4,"y":1},{"x":4,"y":2},{"x":4,"y":1}]],[[{"x":3,"y":2},{"x":4,"y":2},{"x":3,"y":2}]],
@@ -3410,9 +3407,12 @@ const expectedHistoryMoves = {
     [[{"x":2,"y":1},{"x":2,"y":2},{"x":2,"y":3}],[{"x":4,"y":1},{"x":4,"y":0},{"x":4,"y":1}],[{"x":2,"y":2},{"x":2,"y":1},{"x":3,"y":0}],[{"x":4,"y":0},{"x":4,"y":1},{"x":4,"y":0}],[{"x":2,"y":1},{"x":3,"y":1},{"x":4,"y":0}],[{"x":1,"y":0},{"x":1,"y":1},{"x":0,"y":0}],[{"x":3,"y":2},{"x":2,"y":3},{"x":1,"y":4}]],
   ],
 }[puzzle.position];
+console.log(`Solving ${puzzle.position} (#${MinimumGame.fromPosition(puzzle.position).hash}) with depth ${puzzle.maxDepth}, ${expectedHistoryMoves ? `expecting ${expectedHistoryMoves.length} solutions` : 'without knowing how many solutions are there'}`);
+ss1=MinimumGame.solvePosition({...puzzle, usePool: true});
+const allHistoryMoves = (ss1.root.leaves || []).map(game => game.historyFullMoves);
 if (expectedHistoryMoves) {
-  const missingMoves = _.differenceBy(new Set(expectedHistoryMoves), new Set(allHistoryMoves), historyMoves => JSON.stringify(historyMoves));
-  const extraMoves = _.differenceBy(new Set(allHistoryMoves), new Set(expectedHistoryMoves), historyMoves => JSON.stringify(historyMoves));
+  const missingMoves = _.differenceBy(Array.from(new Set(expectedHistoryMoves)), Array.from(new Set(allHistoryMoves)), historyMoves => JSON.stringify(historyMoves));
+  const extraMoves = _.differenceBy(Array.from(new Set(allHistoryMoves)), Array.from(new Set(expectedHistoryMoves)), historyMoves => JSON.stringify(historyMoves));
   if (missingMoves.length || extraMoves.length) {
     throw new Error(`Wrong set of moves returned: ${missingMoves.length} are missing, and ${extraMoves.length} are added`);
   } else {
